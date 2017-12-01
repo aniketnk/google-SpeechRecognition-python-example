6ddd26507d83e76181f41fa01ecf9f07edf75477
aniketnk <aniket.kaulavkar@gmail.com>
2017-12-01 06:16:04 +0530

    Major update, STT and recordAudio become obsolete

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-import time
+# import time
+#
+#
+# print(time.localtime())
+# TimeStamp = list(map(lambda x: str(x) + ' ', time.localtime()))
+# print(TimeStamp)
+# print(int('11 ') + 9)
 
+import speech_recognition as sr
 
-print(time.localtime())
-TimeStamp = list(map(lambda x: str(x) + ' ', time.localtime()))
-print(TimeStamp)
-print(int('11 ') + 9)
+r = sr.Recognizer()
+#source = sr.Microphone(0)
+print("Start Speaking...")
+with sr.Microphone(0) as source:
+    file = r.listen(source, phrase_time_limit= 7)
+print('Sending to google...')
+print(r.recognize_google(file))
